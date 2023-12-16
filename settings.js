26810acf490d8c2b0898a1cfd839cf267f8b3995
Mang D Tech <mangtech24@gmail.com>
2023-12-16 08:25:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'MangDTech'// yourname
 global.MONGO_DB_URI = "mongodb+srv://MangDTech:<password>@cluster0.ijakb.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "&#(3=fGwn20skK&)#)002D#@$" //isi apa aja bebas
-global.your_email = "mangtech24@gmail.com" //email
-global.email_password = "" //application password email
+global.your_email = "ss586593@gmail.com" //email
+global.email_password = "iyza pvxy edfk hzqr" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
